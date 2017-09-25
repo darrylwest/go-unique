24de8e9c81903ae8675f440611a9c3017034c82d
darryl west <darwest@ebay.com>
2017-09-25 14:13:34 -0700

    updates to create client structure to handle requests

--- src/unique-tcp.go
+++ src/unique-tcp.go
@@ -24,10 +24,17 @@ type Server struct {
     IdleTimeout time.Duration
 }
 
-func readRequest(conn net.Conn) (string, error) {
+type Client struct {
+    id string
+    IdleTimeout time.Duration
+    Requests int
+    buffer [64]byte
+}
+
+func (cli *Client) readRequest(conn net.Conn) (string, error) {
     fmt.Println("read from client...")
 
-    ctx, cancel := context.WithTimeout(context.Background(), 2 * time.Minute)
+    ctx, cancel := context.WithTimeout(context.Background(), cli.IdleTimeout)
     defer cancel()
 
     var (
@@ -39,12 +46,13 @@ func readRequest(conn net.Conn) (string, error) {
     complete := make(chan bool, 1)
 
     go func() {
-        buf := make([]byte, 32)
+        buf := cli.buffer[:]
         ccount, err = conn.Read(buf)
-        fmt.Println("ccount:", ccount)
+        // fmt.Println("ccount:", ccount)
         if err == nil && ccount > 0 {
             response = strings.TrimSpace(string(buf[:ccount]))
         }
+        cli.Requests++
 
         complete <- true
     }()
@@ -57,17 +65,17 @@ func readRequest(conn net.Conn) (string, error) {
     }
 }
 
-func (svr Server) handleClient(conn net.Conn) {
+func (cli Client) handleClient(conn net.Conn) {
     defer conn.Close()
     for {
-        buf, err := readRequest(conn)
+        buf, err := cli.readRequest(conn)
         if err != nil {
-            fmt.Printf("connection logs: %s\n", err)
+            fmt.Printf("connection lost from client: %s; %s\n", cli.id, err)
             break
         } else {
             // parse the request
             id := unique.CreateULID()
-            fmt.Printf("request: %s, response: %s\n", buf, id)
+            fmt.Printf("client: %s request: %s, response: %s\n", cli.id, buf, id)
 
             fmt.Fprintf(conn, id)
         }
@@ -98,7 +106,8 @@ func main() {
         }
 
         // create a client struct and add to the list
-        go svr.handleClient(conn)
+        client := Client{ id:unique.CreateTXID(), IdleTimeout:svr.IdleTimeout }
+        go client.handleClient(conn)
     }
 }
 
